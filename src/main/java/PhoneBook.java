7a3619aebd6f96bf4e5820d28974697c7280f3f6
chilledsunset <chilledsunset@gmail.com>
2023-01-18 20:28:34 +0300

    ready

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -10,4 +10,29 @@ public class PhoneBook {
 
         return phoneBook.size();
     }
+
+    public String findByNumber(String phoneNumber) {
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            if (entry.getValue().equals(phoneNumber))
+                return entry.getKey();
+        }
+        return null;
+    }
+
+    public String findByName(String name) {
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            if (entry.getKey().equals(name))
+                return entry.getValue();
+        }
+        return null;
+    }
+
+    public List<String> printAllNames() {
+        List<String> listOfNames = new LinkedList<>();
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            listOfNames.add(entry.getKey());
+            System.out.println(entry.getKey());
+        }
+        return listOfNames;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -36,6 +36,44 @@ public class PhoneBookTest {
         Assertions.assertEquals(expected, result);
     }
 
+    @Test
+    public void testFindByNumber() {
+        // Arrange
+        String expected = "Захар";
+        phoneBook.add("Вениамин", "+79272486999");
+        phoneBook.add("Григорий", "+79063065548");
+        phoneBook.add("Захар", "+79172185570");
+        // Act
+        String result = phoneBook.findByNumber("+79172185570");
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testFindByName() {
+        // Arrange
+        String expected = "+79172185570";
+        phoneBook.add("Вениамин", "+79272486999");
+        phoneBook.add("Григорий", "+79063065548");
+        phoneBook.add("Захар", "+79172185570");
+        // Act
+        String result = phoneBook.findByName("Захар");
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testPrintAllNames() {
+        // Arrange
+        List<String> expected = Arrays.asList("Вениамин", "Григорий", "Захар");
+        phoneBook.add("Вениамин", "+79272486999");
+        phoneBook.add("Григорий", "+79063065548");
+        phoneBook.add("Захар", "+79172185570");
+        // Act
+        List<String> result = phoneBook.printAllNames();
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
 
     @AfterEach
     public void endOfTest() {
