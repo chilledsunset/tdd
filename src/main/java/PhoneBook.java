7f1b5b73aa32f9169a7230a3b17ef4e9b83be66d
chilledsunset <chilledsunset@gmail.com>
2023-01-18 20:26:09 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,13 @@
-public class PhoneBook {
+import java.util.*;
+import java.util.stream.Collectors;
 
-    public boolean add(String name, String phoneNumber) {
-        return false;
+public class PhoneBook {
+    public Map<String, String> phoneBook = new TreeMap<>();
+
+    public int add(String name, String phoneNumber) {
+        if (!phoneBook.containsKey(name))
+            phoneBook.put(name, phoneNumber);
+
+        return phoneBook.size();
     }
 }
