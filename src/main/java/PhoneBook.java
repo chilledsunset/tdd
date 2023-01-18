824c59250ad642b353af6ac4046179ede95e647d
chilledsunset <chilledsunset@gmail.com>
2023-01-18 20:23:49 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+
+    public boolean add(String name, String phoneNumber) {
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,44 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void startOfTest() {
+        System.out.println("Начало теста");
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void testingAddingSingleContact() {
+        //Arrange
+        int expected = 1;
+        // Act
+        int result = phoneBook.add("Григорий", "+79063065548");
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testingAddingTwoContacts() {
+        //Arrange
+        int expected = 2;
+        // Act
+        phoneBook.add("Григорий", "+79063065548");
+        int result = phoneBook.add("Вениамин", "+79272486999");
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+
+    @AfterEach
+    public void endOfTest() {
+        System.out.println("Конец теста");
+    }
 }
